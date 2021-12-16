29ff2930c6c29df56d1217c94832ca855a06464c
ahmed ahmed__daoud <ahmed_daoud@ens.univ-artois.fr>
2021-12-16 16:59:19 +0100

    "Envoi final"

--- resources/views/commentaire/commentaire.blade.php
+++ resources/views/commentaire/commentaire.blade.php
@@ -1,23 +1,28 @@
-@extends('layouts.app')
-
 @section('content')
+    C'est la page générale du site,
+    <br />
+    on doit y voir les dernières séries par exemple.
+    la série {{$serie->nom}}
+    @if (!is_null($user))
+        <form action="store/{{$serie->id}}" method="post">
+            @csrf
+            <div class="form-group">
+                <textarea class="form-control  @error('message') is-invalid @enderror" name="message" id="message" placeholder="Votre message">{{ old('message') }}</textarea>
+                @error('message')
+                <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            <div class="form-group">
+                <textarea class="form-control  @error('note') is-invalid @enderror" name="note" id="note" placeholder="Votre note">{{ old('note') }}</textarea>
+                @error('note')
+                <div class="invalid-feedback">{{ $note }}</div>
+                @enderror
+            </div>
+            <button type="submit" class="btn btn-secondary">Envoyer !</button>
+        </form>
+    @else
+        <p> Veuillez vous connecter</p>
+    @endif
 
-    Poster un commentaire sur la série <b>{{$serie->nom}}</b>
-
-    <form action="store/{{$serie->id}}" method="POST">
-        @csrf
-        <div class="form-group">
-            <textarea class="form-control  @error('message') is-invalid @enderror" name="message" id="message" placeholder="Votre message">{{ old('message') }}</textarea>
-            @error('message')
-            <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-        </div>
-        <div class="form-group">
-            <textarea class="form-control  @error('note') is-invalid @enderror" name="note" id="note" placeholder="Votre note">{{ old('note') }}</textarea>
-            @error('note')
-            <div class="invalid-feedback">{{ $note }}</div>
-            @enderror
-        </div>
-        <button type="submit" class="btn btn-secondary">Envoyer !</button>
-    </form>
 @endsection
+
